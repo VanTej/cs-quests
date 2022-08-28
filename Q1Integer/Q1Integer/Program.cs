73e645eac4034596a01dd6fbc1f818be882dd36a
VanTej <vanessatejedor@gmail.com>
2022-08-28 11:23:48 +0200

    Q1Integer

---
+++ Q1Integer/Q1Integer/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int x = int.MaxValue;
+long y = long.MaxValue;
+short z = short.MaxValue;
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+
+ulong result = (ulong)x + (ulong)y + (ulong)z;
+Console.WriteLine(result);
