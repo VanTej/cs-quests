f2e55ecc7db4e02b93c1911b25847aad673c4a66
VanTej <vanessatejedor@gmail.com>
2022-08-28 11:34:47 +0200

    Q1List

---
+++ Q1List/Q1List/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<string> names = new List<string>();
+names.Add("Vanessa");
+names.Add("Vaness");
+names.Add("Van");
+foreach (string item in names)
+{
+    Console.WriteLine($"Hello, {item}!");
+}
+names[1] = "VANESSA";
+names.RemoveAt(0);
+names.Remove("Van");
+foreach (string item in names)
+{
+    Console.WriteLine($"Bye, {item}!");
+}
