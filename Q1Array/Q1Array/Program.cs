3bdf53063bb598cd6130970e7dcabc9a18442bb4
VanTej <vanessatejedor@gmail.com>
2022-08-30 10:47:49 +0200

    Q1Array

---
+++ Q1Array/Q1Array/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] unity = new int[5];
+unity[0] = 0;
+unity[1] = 1;
+unity[2] = 2;
+unity[3] = 3;
+unity[4] = 4;
+int[] dec = new int[5];
+dec[0] = 10;
+dec[1] = 11;
+dec[2] = 12;
+dec[3] = 13;
+dec[4] = 14;
+int[] fusion = new int[10];
+for (int i = 0; i < unity.Length; i++)
+{
+	fusion[i] = unity[i];
+	fusion[i + unity.Length] = dec[i];
+
+}
+
+foreach (var item in fusion)
+{
+	Console.WriteLine(item);
+}
--- Q1Solid/Q1Solid/Program.cs
+++ Q1Solid/Q1Solid/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Q1Solid
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    internal class Polygon
+    {
+        private double width;
+        private double height;
+
+        public void SetWidth(double width)
+        {
+            this.width = width;
+        }
+
+        public void SetHeight(double height)
+        {
+            this.height = height;
+        }
+
+        public double GetWidth()
+        {
+            return width;
+        }
+
+        public double GetHeight()
+        {
+            return height;
+        }
+
+        public double GetArea()
+        {
+            return width * height;
+        }
+    }
+
+    internal class Square : Polygon
+    {
+    }
+
+    internal class Rectangle : Polygon
+    {
+    }
+}
